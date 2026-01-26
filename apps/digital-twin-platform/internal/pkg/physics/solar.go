4b20e57cee07e523f9dcab9df1171945029c7879
dimbo1324 <dimaprihodko180@gmail.com>
2026-01-26 14:49:02 +0300

    Refactor SolarIrradiance function for improved readability by removing unnecessary line breaks

--- apps/digital-twin-platform/internal/pkg/physics/solar.go
+++ apps/digital-twin-platform/internal/pkg/physics/solar.go
@@ -7,21 +7,15 @@ import (
 
 func SolarIrradiance(t time.Time, peakIrradiance, sunriseHour, sunsetHour float64) float64 {
 	currentHour := float64(t.Hour()) + float64(t.Minute())/60.0
-
 	if currentHour < sunriseHour || currentHour > sunsetHour {
 		return 0.0
 	}
-
 	dayDuration := sunsetHour - sunriseHour
-
 	radians := (currentHour - sunriseHour) / dayDuration * math.Pi
-
 	irradiance := peakIrradiance * math.Sin(radians)
-
 	if irradiance < 0 {
 		return 0
 	}
-
 	return irradiance
 }
 
